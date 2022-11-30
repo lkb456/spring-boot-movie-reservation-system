99c33a721c23a07b53be341c85a2afd600d5ae99
이경범 <tkdwk567@naver.com>
2022-11-30 15:22:51 +0900

    Initial commit

---
+++ src/test/java/com/example/springbootmoviereservationsystem/SpringBootMovieReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmoviereservationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMovieReservationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
