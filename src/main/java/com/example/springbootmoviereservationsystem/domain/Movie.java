3f6fdac4bba86bb1751fcc8e188f422ee5c78e79
이경범 <tkdwk567@naver.com>
2022-11-30 17:59:55 +0900

    [refactor] 영화 도메인 속성 추가 및 리파지토리 생성

--- src/main/java/com/example/springbootmoviereservationsystem/domain/Movie.java
+++ src/main/java/com/example/springbootmoviereservationsystem/domain/Movie.java
@@ -1,12 +1,28 @@
 package com.example.springbootmoviereservationsystem.domain;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
+import java.time.Duration;
+
+@Entity
+@Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class Movie {
 
-    private Long fee;
-    private ReleaseStatus releaseStatus;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long movieId; // 순번
+
+    private String title; // 제목
+    private Long fee; // 요금
+    private Duration runningTime; // 상영 시간
+
+    @Enumerated(value = EnumType.STRING)
+    private ReleaseStatus releaseStatus; // 개봉 상태
 
     public void changeReleaseStatus(ReleaseStatus releaseStatus) {
         this.releaseStatus = releaseStatus;
