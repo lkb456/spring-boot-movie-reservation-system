1e7a2bb16a859a28824b50b8064bf4491ecd129a
이경범 <tkdwk567@naver.com>
2022-11-30 16:26:21 +0900

    [feat] 영화 예매 기능 add

--- src/main/java/com/example/springbootmoviereservationsystem/domain/Movie.java
+++ src/main/java/com/example/springbootmoviereservationsystem/domain/Movie.java
@@ -2,12 +2,22 @@ package com.example.springbootmoviereservationsystem.domain;
 
 public class Movie {
 
+    private Long fee;
     private ReleaseStatus releaseStatus;
+
     public void changeReleaseStatus(ReleaseStatus releaseStatus) {
         this.releaseStatus = releaseStatus;
     }
 
     public boolean isReleaseMovie() {
         return this.releaseStatus.isRelease(ReleaseStatus.RELEASE);
+    }
+
+    public Long calculateMovieFee(int audienceCount) {
+        return multiply(audienceCount);
+    }
+
+    private Long multiply(int audienceCount) {
+        return this.fee * audienceCount;
     }
 }
--- src/main/java/com/example/springbootmoviereservationsystem/domain/Reservation.java
+++ src/main/java/com/example/springbootmoviereservationsystem/domain/Reservation.java
@@ -1,4 +1,14 @@
 package com.example.springbootmoviereservationsystem.domain;
 
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED, staticName = "of")
 public class Reservation {
+
+    private final Consumer consumer;
+    private final int audienceCount;
+    private final Movie movie;
+    private final Long fee;
+
 }
