a0ea380a974e9fe7ffb013c4645d377cb02c5398
이경범 <tkdwk567@naver.com>
2022-11-30 20:37:42 +0900

    [feat] 고객 정보 등록 api 추가
    중복된 휴대전화 번호가 있을 시 예외 처리

--- src/main/java/com/example/springbootmoviereservationsystem/domain/consumer/Consumer.java
+++ src/main/java/com/example/springbootmoviereservationsystem/domain/consumer/Consumer.java
@@ -1,17 +1,24 @@
 package com.example.springbootmoviereservationsystem.domain.consumer;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Consumer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nickName;
+
+    @Column(unique = true)
     private String phoneNumber;
 
 }
---
+++ src/main/java/com/example/springbootmoviereservationsystem/service/ConsumerService.java
@@ -0,0 +1,30 @@
+package com.example.springbootmoviereservationsystem.service;
+
+import com.example.springbootmoviereservationsystem.controller.dto.ConsumerSaveRequestDto;
+import com.example.springbootmoviereservationsystem.domain.consumer.Consumer;
+import com.example.springbootmoviereservationsystem.domain.consumer.ConsumerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ConsumerService {
+
+    private final ConsumerRepository consumerRepository;
+
+    public Long saveConsumer(ConsumerSaveRequestDto consumerSaveRequestDto) {
+        duplicateCheck(consumerSaveRequestDto);
+        Consumer savedConsumer = consumerRepository.save(consumerSaveRequestDto.toEntity());
+        return savedConsumer.getId();
+    }
+
+    private void duplicateCheck(ConsumerSaveRequestDto consumerSaveRequestDto) {
+        if (isExists(consumerSaveRequestDto.getPhoneNumber())) {
+            throw new IllegalArgumentException("중복된 전화번호 입니다.");
+        }
+    }
+
+    private boolean isExists(String phoneNumber) {
+        return consumerRepository.existsByPhoneNumber(phoneNumber);
+    }
+}
