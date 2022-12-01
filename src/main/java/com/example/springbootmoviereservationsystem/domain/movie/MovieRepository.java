e9a7e28841cdb0284106dada7f7546766c5f89ac
이경범 <tkdwk567@naver.com>
2022-12-01 15:46:20 +0900

    [remove] import 문 최적화

--- src/main/java/com/example/springbootmoviereservationsystem/domain/movie/MovieRepository.java
+++ src/main/java/com/example/springbootmoviereservationsystem/domain/movie/MovieRepository.java
@@ -1,6 +1,5 @@
 package com.example.springbootmoviereservationsystem.domain.movie;
 
-import com.example.springbootmoviereservationsystem.domain.movie.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
