4610125906c922e97ab24925e3b02cc39f9ce458
이경범 <tkdwk567@naver.com>
2022-12-01 22:21:01 +0900

    [refactor] 리파지토리에서 옵셔널인 엔티티를 가져올 때의 중복된 코드 리펙토링

--- src/main/java/com/example/springbootmoviereservationsystem/service/ReservationService.java
+++ src/main/java/com/example/springbootmoviereservationsystem/service/ReservationService.java
@@ -3,11 +3,9 @@ package com.example.springbootmoviereservationsystem.service;
 import com.example.springbootmoviereservationsystem.controller.dto.request.ReservationSaveRequestDto;
 import com.example.springbootmoviereservationsystem.controller.dto.response.ReservationSaveResponseDto;
 import com.example.springbootmoviereservationsystem.domain.consumer.Consumer;
-import com.example.springbootmoviereservationsystem.domain.consumer.ConsumerRepository;
 import com.example.springbootmoviereservationsystem.domain.reservation.Reservation;
 import com.example.springbootmoviereservationsystem.domain.reservation.ReservationRepository;
 import com.example.springbootmoviereservationsystem.domain.screening.Screening;
-import com.example.springbootmoviereservationsystem.domain.screening.ScreeningRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,27 +14,16 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ReservationService {
 
-    private final ConsumerRepository consumerRepository;
-    private final ScreeningRepository screeningRepository;
     private final ReservationRepository reservationRepository;
+    private final ConsumerService consumerService;
+    private final ScreeningService screeningService;
 
     @Transactional
     public ReservationSaveResponseDto reserveSave(Long screeningId, ReservationSaveRequestDto reservationSaveRequestDto) {
-        Screening screening = findScreen(screeningId);
-        Reservation reservation = screening.reserve(
-                findConsumer(reservationSaveRequestDto),
-                reservationSaveRequestDto.getAudienceCount());
+        Consumer consumer = consumerService.findConsumer(reservationSaveRequestDto.getPhoneNumber());
+        Screening screening = screeningService.findScreen(screeningId);
+        Reservation reservation = screening.reserve(consumer, reservationSaveRequestDto.getAudienceCount());
         Reservation savedReservation = reservationRepository.save(reservation);
         return ReservationSaveResponseDto.of(savedReservation);
-    }
-
-    private Screening findScreen(Long screeningId) {
-        return screeningRepository.findById(screeningId)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 상영정보입니다."));
-    }
-
-    private Consumer findConsumer(ReservationSaveRequestDto reservationSaveRequestDto) {
-        return consumerRepository.findByPhoneNumber(reservationSaveRequestDto.getPhoneNumber())
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다."));
     }
 }
