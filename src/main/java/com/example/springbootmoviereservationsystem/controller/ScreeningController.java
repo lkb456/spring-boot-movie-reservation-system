d1c3d858ef002c6e765c25f5941eea37c7ebd38a
이경범 <tkdwk567@naver.com>
2022-12-01 15:30:20 +0900

    [feat] 상영 영화 등록 api 추가

---
+++ src/main/java/com/example/springbootmoviereservationsystem/controller/ScreeningController.java
@@ -0,0 +1,31 @@
+package com.example.springbootmoviereservationsystem.controller;
+
+import com.example.springbootmoviereservationsystem.controller.dto.ScreeningSaveResponseDto;
+import com.example.springbootmoviereservationsystem.service.ScreeningService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@Valid
+@RestController
+@RequiredArgsConstructor
+public class ScreeningController {
+
+    private final ScreeningService screeningService;
+
+    @PostMapping("/movies/{id}/screenings")
+    public ResponseEntity<ScreeningSaveResponseDto> screenSave(@PathVariable(name = "id") Long movieId,
+                                     @RequestParam("when")
+                                     @DateTimeFormat(pattern = "yyyy-MM-dd-HH:mm") LocalDateTime whenScreened) {
+        ScreeningSaveResponseDto screeningSaveResponseDto = screeningService.saveScreen(movieId, whenScreened);
+        return ResponseEntity.status(HttpStatus.CREATED).body(screeningSaveResponseDto);
+    }
+}
---
+++ src/main/java/com/example/springbootmoviereservationsystem/service/ScreeningService.java
@@ -0,0 +1,43 @@
+package com.example.springbootmoviereservationsystem.service;
+
+import com.example.springbootmoviereservationsystem.controller.dto.MovieSaveResponseDto;
+import com.example.springbootmoviereservationsystem.controller.dto.ScreeningSaveResponseDto;
+import com.example.springbootmoviereservationsystem.domain.movie.Movie;
+import com.example.springbootmoviereservationsystem.domain.movie.MovieRepository;
+import com.example.springbootmoviereservationsystem.domain.screening.Screening;
+import com.example.springbootmoviereservationsystem.domain.screening.ScreeningRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class ScreeningService {
+
+    private final MovieRepository movieRepository;
+    private final ScreeningRepository screeningRepository;
+
+    @Transactional
+    public ScreeningSaveResponseDto saveScreen(Long movieId, LocalDateTime whenScreened) {
+        Movie movie = movieFind(movieId);
+        Screening savedScreening = screenSave(whenScreened, movie);
+        return ScreeningSaveResponseDto.of(savedScreening.getId(),
+                MovieSaveResponseDto.of(movie),
+                savedScreening.getWhenScreened());
+
+    }
+
+    private Screening screenSave(LocalDateTime whenScreened, Movie movie) {
+        return screeningRepository.save(Screening.builder()
+                .movie(movie)
+                .whenScreened(whenScreened)
+                .build());
+    }
+
+    private Movie movieFind(Long movieId) {
+        return movieRepository.findById(movieId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 영화입니다."));
+    }
+}
