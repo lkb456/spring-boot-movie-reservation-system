92381774ec155c28f4e093ecbdd7e4bcafb397f4
이경범 <tkdwk567@naver.com>
2022-12-01 21:03:57 +0900

    [feat] 예매 등록 api 추가

---
+++ src/main/java/com/example/springbootmoviereservationsystem/controller/dto/response/ReservationSaveResponseDto.java
@@ -0,0 +1,34 @@
+package com.example.springbootmoviereservationsystem.controller.dto.response;
+
+import com.example.springbootmoviereservationsystem.domain.reservation.Reservation;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Builder
+@Getter
+@RequiredArgsConstructor
+public class ReservationSaveResponseDto {
+
+    @JsonProperty("consumer")
+    private final ConsumerSaveResponseDto consumerSaveResponseDto;
+
+    @JsonProperty("audience_count")
+    private final int audienceCount;
+
+    @JsonProperty("screening")
+    private final ScreeningSaveResponseDto screeningSaveResponseDto;
+
+    @JsonProperty("total_fee")
+    private final Long fee;
+
+    public static ReservationSaveResponseDto of(Reservation savedReservation) {
+        return ReservationSaveResponseDto.builder()
+                .consumerSaveResponseDto(ConsumerSaveResponseDto.of(savedReservation.getConsumer()))
+                .audienceCount(savedReservation.getAudienceCount())
+                .screeningSaveResponseDto(ScreeningSaveResponseDto.of(savedReservation.getScreening()))
+                .fee(savedReservation.getFee())
+                .build();
+    }
+}
