2b1b4ad9e5365fd417ca5d1da8983d4bbb28e621
이경범 <tkdwk567@naver.com>
2022-12-01 15:45:32 +0900

    [add] 상영 도메인 pk 값 IDENTITY 설정 및 movie 객체 지연 로딩으로 변경

--- src/main/java/com/example/springbootmoviereservationsystem/domain/screening/Screening.java
+++ src/main/java/com/example/springbootmoviereservationsystem/domain/screening/Screening.java
@@ -3,25 +3,26 @@ package com.example.springbootmoviereservationsystem.domain.screening;
 import com.example.springbootmoviereservationsystem.domain.consumer.Consumer;
 import com.example.springbootmoviereservationsystem.domain.movie.Movie;
 import com.example.springbootmoviereservationsystem.domain.reservation.Reservation;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Screening {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private Movie movie;
     private LocalDateTime whenScreened;
 
