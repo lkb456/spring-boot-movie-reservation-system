3097d8bf4fcd6fd23da7fa1bd6ef2f3a79a28ea6
이경범 <tkdwk567@naver.com>
2022-12-02 16:24:05 +0900

    [correct] 도메인 컬럼명 컨벤션 적용 -> 대문자와 복수형

--- src/main/java/com/example/springbootmoviereservationsystem/domain/movie/Movie.java
+++ src/main/java/com/example/springbootmoviereservationsystem/domain/movie/Movie.java
@@ -10,25 +10,34 @@ import org.hibernate.annotations.CreationTimestamp;
 import java.time.Duration;
 import java.time.LocalDateTime;
 
-@Entity
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "MOVIES")
 public class Movie {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MOVIES_ID")
     private Long id; // 순번
 
+    @Column(name = "TITLE")
     private String title; // 제목
+
+    @Column(name = "FEE")
     private Long fee; // 요금
+
+    @Column(name = "RUNNING_TIME")
     private Duration runningTime; // 상영 시간
 
     @Enumerated(value = EnumType.STRING)
+    @Column(name = "RELEASE_STATUS")
     private ReleaseStatus releaseStatus; // 개봉 상태
 
     @CreationTimestamp
+    @Column(name = "CREATE_AT")
     private LocalDateTime createAt;
 
     public void changeReleaseStatus(ReleaseStatus releaseStatus) {
