9496124a517df5207883f8d5eb3791772a825707
이경범 <tkdwk567@naver.com>
2022-12-02 18:27:09 +0900

    [refactor] 티켓 정보가 없는 경우 json 형식에 포함하지 않게 설정

--- src/main/java/com/example/springbootmoviereservationsystem/controller/dto/response/ConsumerSaveResponseDto.java
+++ src/main/java/com/example/springbootmoviereservationsystem/controller/dto/response/ConsumerSaveResponseDto.java
@@ -1,12 +1,16 @@
 package com.example.springbootmoviereservationsystem.controller.dto.response;
 
-import com.example.springbootmoviereservationsystem.domain.ticket.Ticket;
 import com.example.springbootmoviereservationsystem.domain.consumer.Consumer;
+import com.example.springbootmoviereservationsystem.domain.ticket.Ticket;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
+@AllArgsConstructor
 public class ConsumerSaveResponseDto {
 
     private final String nickname; // 고객 닉네임
@@ -14,14 +18,8 @@ public class ConsumerSaveResponseDto {
     @JsonProperty("phone")
     private final String phoneNumber; // 고객 핸드폰 번호
 
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
     private final Ticket ticket; // 티켓
-
-    @Builder
-    public ConsumerSaveResponseDto(String nickname, String phoneNumber, Ticket ticket) {
-        this.nickname = nickname;
-        this.phoneNumber = phoneNumber;
-        this.ticket = ticket;
-    }
 
     public static ConsumerSaveResponseDto of(Consumer consumer) {
          return ConsumerSaveResponseDto.builder()
