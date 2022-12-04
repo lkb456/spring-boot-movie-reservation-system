28045ae5be76f50033fb8520c67da975e32e3bb6
이경범 <tkdwk567@naver.com>
2022-12-04 19:14:02 +0900

    [feat] 영화 정보 수정 api 추가

--- src/main/java/com/example/springbootmoviereservationsystem/controller/MovieController.java
+++ src/main/java/com/example/springbootmoviereservationsystem/controller/MovieController.java
@@ -1,6 +1,7 @@
 package com.example.springbootmoviereservationsystem.controller;
 
 import com.example.springbootmoviereservationsystem.controller.dto.request.MovieSaveRequestDto;
+import com.example.springbootmoviereservationsystem.controller.dto.request.MovieUpdateRequestDto;
 import com.example.springbootmoviereservationsystem.controller.dto.response.MovieSaveResponseDto;
 import com.example.springbootmoviereservationsystem.domain.movie.Movie;
 import com.example.springbootmoviereservationsystem.domain.movie.MovieRepository;
@@ -8,8 +9,10 @@ import com.example.springbootmoviereservationsystem.service.MovieService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
 public class MovieController {
@@ -27,5 +30,12 @@ public class MovieController {
     public ResponseEntity<MovieSaveResponseDto> movieFind(@PathVariable("id") Long movieId) {
         MovieSaveResponseDto movieSaveResponseDto = MovieSaveResponseDto.of(movieService.findMovie(movieId));
         return ResponseEntity.status(HttpStatus.OK).body(movieSaveResponseDto);
+    }
+
+    @PutMapping("/movies/{id}")
+    public ResponseEntity<Long> movieUpdate(@PathVariable("id") Long movieId,
+                                            @RequestBody MovieUpdateRequestDto movieUpdateRequestDto) {
+        Long updatedMovieId = movieService.updateMovie(movieId, movieUpdateRequestDto);
+        return ResponseEntity.status(HttpStatus.OK).body(updatedMovieId);
     }
 }
