2d131304549a7112127f28273a37c1e5e3395bb2
이경범 <tkdwk567@naver.com>
2022-12-06 14:30:48 +0900

    [feat] 상영영화 검색 api 추가

--- src/main/java/com/example/springbootmoviereservationsystem/controller/ScreeningController.java
+++ src/main/java/com/example/springbootmoviereservationsystem/controller/ScreeningController.java
@@ -1,15 +1,15 @@
 package com.example.springbootmoviereservationsystem.controller;
 
+import com.example.springbootmoviereservationsystem.controller.dto.PageMovieResponseDto;
 import com.example.springbootmoviereservationsystem.controller.dto.response.ScreeningSaveResponseDto;
 import com.example.springbootmoviereservationsystem.service.ScreeningService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.time.LocalDateTime;
@@ -28,5 +28,13 @@ public class ScreeningController {
         ScreeningSaveResponseDto screeningSaveResponseDto =
                 ScreeningSaveResponseDto.of(screeningService.saveScreen(movieId, whenScreened));
         return ResponseEntity.status(HttpStatus.CREATED).body(screeningSaveResponseDto);
+    }
+
+    @GetMapping("/screenings")
+    public ResponseEntity<PageMovieResponseDto> screenFind(@RequestParam(value = "title", required = false) String title,
+                                                           @RequestParam(value = "when", required = false) LocalDateTime whenScreened,
+                                                           @PageableDefault Pageable pageable) {
+        PageMovieResponseDto pageMovieResponseDto = screeningService.searchScreening(title, whenScreened, pageable);
+        return ResponseEntity.status(HttpStatus.OK).body(pageMovieResponseDto);
     }
 }
--- src/main/java/com/example/springbootmoviereservationsystem/service/ScreeningService.java
+++ src/main/java/com/example/springbootmoviereservationsystem/service/ScreeningService.java
@@ -1,11 +1,12 @@
 package com.example.springbootmoviereservationsystem.service;
 
+import com.example.springbootmoviereservationsystem.controller.dto.PageMovieResponseDto;
 import com.example.springbootmoviereservationsystem.domain.movie.Movie;
 import com.example.springbootmoviereservationsystem.domain.screening.Screening;
 import com.example.springbootmoviereservationsystem.domain.screening.ScreeningRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
@@ -16,15 +17,18 @@ public class ScreeningService {
     private final ScreeningRepository screeningRepository;
     private final MovieService movieService;
 
-    @Transactional
     public Screening saveScreen(Long movieId, LocalDateTime whenScreened) {
         Movie movie = movieService.findMovie(movieId);
         return screenSave(whenScreened, movie);
     }
 
-    public Screening findScreen(Long screenId) {
-        return screeningRepository.findById(screenId)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 상영정보입니다."));
+    public Screening findScreen(Long screeningId) {
+        return screeningRepository.findById(screeningId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 상영정보 입니다."));
+    }
+
+    public PageMovieResponseDto searchScreening(String title, LocalDateTime startTime, Pageable pageable) {
+        return PageMovieResponseDto.of(screeningRepository.findScreeningStartTimeAfterAndTitle(title, startTime, pageable));
     }
 
     private Screening screenSave(LocalDateTime whenScreened, Movie movie) {
