0a508f1549bc0b15deb666876ecf96b04cc37554
이경범 <tkdwk567@naver.com>
2022-12-16 18:09:08 +0900

    [remove] 메서드 제거

--- src/main/java/com/example/springbootmoviereservationsystem/domain/repository/ConsumerRepository.java
+++ src/main/java/com/example/springbootmoviereservationsystem/domain/repository/ConsumerRepository.java
@@ -4,10 +4,7 @@ import com.example.springbootmoviereservationsystem.domain.Consumer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
 @Transactional(readOnly = true)
 public interface ConsumerRepository extends JpaRepository<Consumer, Long> {
     boolean existsByPhoneNumber(String phoneNumber);
-    Optional<Consumer> findByPhoneNumber(String phoneNumber);
 }
