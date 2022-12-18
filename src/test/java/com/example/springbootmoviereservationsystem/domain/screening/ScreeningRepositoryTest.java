5a841e30958e3cc842ec6e758f349ebdd2c328d4
이경범 <tkdwk567@naver.com>
2022-12-19 00:06:05 +0900

    [test] ScreeningRepository 페이지 쿼리 메서드 test

---
+++ src/test/java/com/example/springbootmoviereservationsystem/domain/screening/ScreeningRepositoryTest.java
@@ -0,0 +1,55 @@
+package com.example.springbootmoviereservationsystem.domain.screening;
+
+import com.example.springbootmoviereservationsystem.controller.screening.dto.ScreenDtoProjection;
+import com.example.springbootmoviereservationsystem.domain.movie.Movie;
+import com.example.springbootmoviereservationsystem.domain.movie.MovieRepository;
+import com.example.springbootmoviereservationsystem.fixture.CreateEntity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@DataJpaTest
+class ScreeningRepositoryTest {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    @Autowired
+    private ScreeningRepository screeningRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        Movie movie = movieRepository.save(CreateEntity.createMovie());
+        screeningRepository.save(CreateEntity.createScreening(movie));
+    }
+
+    @Test
+    @DisplayName("페이지 처리 쿼리 메서드 테스트")
+    void ScreeningRepositoryTest() {
+        // given
+        String title = "아";
+        LocalDateTime when = LocalDateTime.of(2022, 12, 01, 8, 00);
+        Pageable pageable = Pageable.unpaged();
+
+        Screening screening = screeningRepository.findById(1L).get();
+
+        // when
+        Page<ScreenDtoProjection> result = screeningRepository.findByMovieTitleStartingWithAndWhenScreenedGreaterThanEqual(title, when, pageable);
+
+        // then
+        List<ScreenDtoProjection> content = result.getContent();
+        assertNotNull(result);
+        assertThat(screening.getMovie().getTitle()).isEqualTo(content.get(0).getMovie().getTitle());
+        assertThat(screening.getWhenScreened()).isEqualTo(content.get(0).getWhenScreened());
+    }
+}
