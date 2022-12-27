19accd3b59195d687912eecedc8c2b2e41fcdee0
이경범 <tkdwk567@naver.com>
2022-12-27 21:57:59 +0900

    [chore] comment 제거

--- src/main/java/com/example/springbootmoviereservationsystem/service/MovieService.java
+++ src/main/java/com/example/springbootmoviereservationsystem/service/MovieService.java
@@ -7,7 +7,6 @@ import com.example.springbootmoviereservationsystem.domain.money.Money;
 import com.example.springbootmoviereservationsystem.domain.movie.Movie;
 import com.example.springbootmoviereservationsystem.domain.movie.MovieRepository;
 import com.example.springbootmoviereservationsystem.domain.movie.ReleaseStatus;
-import com.example.springbootmoviereservationsystem.infra.policy.AmountDiscountPolicy;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -20,14 +19,9 @@ import org.springframework.transaction.annotation.Transactional;
 public class MovieService {
 
     private final MovieRepository movieRepository;
-    private final AmountDiscountPolicy discountPolicy;
 
-    // 할인 금액 반영 안됨
-    // TODO money 엔티티를 value 타입으로 변경
-    // money 는 value 타입이 적절하다
     public Long saveMovie(MovieRequestDto.MovieSaveDto movieSaveRequestDto) {
         Movie movie = movieSaveRequestDto.toEntity();
-        movie.changeDiscountPolicy(discountPolicy);
         return movieRepository.save(movie).getId();
     }
 
