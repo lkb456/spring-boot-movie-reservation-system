ce80e9f46b51cc53ec18bd52fc0e215d6e790e35
이경범 <tkdwk567@naver.com>
2023-01-12 01:39:44 +0900

    [feat] actor 정보 저장 기능 추가

---
+++ src/main/java/com/example/springbootmoviereservationsystem/controller/actor/ActorController.java
@@ -0,0 +1,24 @@
+package com.example.springbootmoviereservationsystem.controller.actor;
+
+import com.example.springbootmoviereservationsystem.service.actor.ActorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+public class ActorController {
+
+    private final ActorService actorService;
+
+    @PostMapping("/actors")
+    public ResponseEntity<ActorResponseDto> createActor(@Valid @RequestBody ActorRequestDto actorRequestDto) {
+        ActorResponseDto actorResponseDto = actorService.saveActor(actorRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(actorResponseDto);
+    }
+}
---
+++ src/main/java/com/example/springbootmoviereservationsystem/controller/actor/ActorResponseDto.java
@@ -0,0 +1,32 @@
+package com.example.springbootmoviereservationsystem.controller.actor;
+
+import com.example.springbootmoviereservationsystem.domain.actor.Actor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@Builder
+@RequiredArgsConstructor
+public class ActorResponseDto {
+
+    private final Long id;
+    private final String firstName;
+    private final String lastName;
+    private final LocalDate birthDate;
+    private final String image;
+    private final String content;
+
+    public static ActorResponseDto of(Actor actor) {
+        return ActorResponseDto.builder()
+                .id(actor.getId())
+                .firstName(actor.getFirstName())
+                .lastName(actor.getLastName())
+                .birthDate(actor.getBirthDate())
+                .image(actor.getImage())
+                .content(actor.getContent())
+                .build();
+    }
+}
