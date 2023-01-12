0a1dda08a69d393a09efa72da992ce956e35cc52
이경범 <tkdwk567@naver.com>
2023-01-12 22:38:28 +0900

    [refactor] file 경로 상수 클래스로 관리

---
+++ src/main/java/com/example/movie_reservation/infra/FilePath.java
@@ -0,0 +1,8 @@
+package com.example.movie_reservation.infra;
+
+public final class FilePath {
+
+    public static final String SEAT_FILE_FULL_PATH = "src/main/resources/static/seat/seat_info.txt";
+    public static final String PLACE_FILE_FULL_PATH = "src/main/resources/static/place/place_info.txt";
+
+}
