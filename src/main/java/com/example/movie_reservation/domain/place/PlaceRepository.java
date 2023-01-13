a856b9e36fa0438366009165ce1ab47d3d54f1bc
이경범 <tkdwk567@naver.com>
2023-01-13 16:49:46 +0900

    [remove] 구조 변경으로 제거

--- src/main/java/com/example/movie_reservation/domain/place/PlaceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.movie_reservation.domain.place;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.transaction.annotation.Transactional;
-
-@Transactional(readOnly = true)
-public interface PlaceRepository extends JpaRepository<Place, Long> {
-}
--- src/main/java/com/example/movie_reservation/service/FileReaderService.java
+++
@@ -1,8 +0,0 @@
-package com.example.movie_reservation.service;
-
-import java.util.List;
-
-public interface FileReaderService<T> {
-    public void fileReader();
-    public List<String> getData();
-}
