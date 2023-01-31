46667d94ef22d0ca854924676f7f7b59531ed4ed
이경범 <tkdwk567@naver.com>
2023-01-31 14:08:04 +0900

    [add] 상영영화 API 문서화

--- src/main/java/com/example/movie_reservation/controller/screening/ScreeningController.java
+++ src/main/java/com/example/movie_reservation/controller/screening/ScreeningController.java
@@ -4,6 +4,11 @@ import com.example.movie_reservation.controller.screening.dto.PageScreenResponse
 import com.example.movie_reservation.controller.screening.dto.ScreeningSaveRequestDto;
 import com.example.movie_reservation.controller.screening.dto.ScreeningSaveResponseDto;
 import com.example.movie_reservation.service.ScreeningService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -16,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.time.LocalDateTime;
 
+@Api(tags = "영화 상영정보 API")
 @Validated
 @RestController
 @RequiredArgsConstructor
@@ -24,16 +30,30 @@ public class ScreeningController {
     private final ScreeningService screeningService;
 
     @PostMapping("/screenings")
-    public ResponseEntity<ScreeningSaveResponseDto> screenSave(@Valid @RequestBody final ScreeningSaveRequestDto screeningSaveRequestDto) {
+    @Operation(summary = "영화 상영정보 생성", description = "영화 식별자와, 상영 시간을 저장한다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "영화 상영정보 생성 성공"),
+            @ApiResponse(code = 400, message = "요청 데이터 유효성 검사 실패")
+    })
+    public ResponseEntity<ScreeningSaveResponseDto> screenSave(@Valid
+                                                               @RequestBody
+                                                               @ApiParam(value = "영화 상영정보") final ScreeningSaveRequestDto screeningSaveRequestDto) {
         ScreeningSaveResponseDto screeningSaveResponseDto =
                 ScreeningSaveResponseDto.of(screeningService.saveScreen(screeningSaveRequestDto));
         return ResponseEntity.status(HttpStatus.CREATED).body(screeningSaveResponseDto);
     }
 
     @GetMapping("/screenings")
-    public ResponseEntity<PageScreenResponseDto> screensSearch(@RequestParam(value = "title", required = false) final String title,
+    @Operation(summary = "영화 상영정보 페이징", description = "해당 키워드를 포함하고 있는 상영영화 정보와 선택 시간 이후에 있는 상영영화 정보를 페이징 처리하여 조회한다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "영화 상영정보 페이징 성공"),
+            @ApiResponse(code = 400, message = "요청 데이터 유효성 검사 실패")
+    })
+    public ResponseEntity<PageScreenResponseDto> screensSearch(@RequestParam(value = "title", required = false)
+                                                               @ApiParam(value = "키워드") final String title,
                                                                @RequestParam(value = "when", required = false)
-                                                               @DateTimeFormat(pattern = "yyyy-MM-dd-HH:mm") final LocalDateTime whenScreened,
+                                                               @DateTimeFormat(pattern = "yyyy-MM-dd-HH:mm")
+                                                               @ApiParam(value = "상영 시간") final LocalDateTime whenScreened,
                                                                @PageableDefault Pageable pageable) {
         PageScreenResponseDto pageScreenResponseDto = screeningService.searchScreens(title, whenScreened, pageable);
         return ResponseEntity.status(HttpStatus.OK).body(pageScreenResponseDto);
--- src/main/java/com/example/movie_reservation/controller/screening/dto/ScreeningSaveRequestDto.java
+++ src/main/java/com/example/movie_reservation/controller/screening/dto/ScreeningSaveRequestDto.java
@@ -2,6 +2,8 @@ package com.example.movie_reservation.controller.screening.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,6 +12,7 @@ import lombok.NoArgsConstructor;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
+@ApiModel(value = "영화 상영정보", description = "영화 식별자, 상영 시간")
 @Getter
 @Builder
 @NoArgsConstructor
@@ -18,9 +21,11 @@ public class ScreeningSaveRequestDto {
 
     @NotNull
     @JsonProperty("movie_id")
+    @ApiModelProperty(value = "영화 식별자", example = "1")
     private Long movieId;
 
     @NotNull
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd-HH:mm")
+    @ApiModelProperty(value = "상영 시간", example = "2022-01-01-08:30")
     private LocalDateTime when;
 }
