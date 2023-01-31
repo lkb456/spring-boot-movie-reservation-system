aa7cef81ac7541cd58289f4d4b77d1cf1e580e40
이경범 <tkdwk567@naver.com>
2023-01-31 14:07:46 +0900

    [add] 영화배우 API 문서화

--- src/main/java/com/example/movie_reservation/controller/actor/dto/ActorRequestDto.java
+++ src/main/java/com/example/movie_reservation/controller/actor/dto/ActorRequestDto.java
@@ -3,12 +3,15 @@ package com.example.movie_reservation.controller.actor.dto;
 import com.example.movie_reservation.domain.actor.Actor;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
+@ApiModel(value = "영화배우 정보", description = "성, 이름, 생년월일, 사진, 이력내용")
 @Getter
 @Builder
 @NoArgsConstructor
@@ -17,24 +20,29 @@ public class ActorRequestDto {
 
     @NotBlank
     @JsonProperty("first_name")
-    private String firstName; // 성
+    @ApiModelProperty(value = "이름", example = "동석")
+    private String firstName;
 
     @NotBlank
     @JsonProperty("last_name")
-    private String lastName; // 이름
+    @ApiModelProperty(value = "성", example = "마")
+    private String lastName;
 
     @NotNull
     @JsonProperty("birth_date")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    private LocalDate birthDate; // 생년월일
+    @ApiModelProperty(value = "생년월일", example = "2022-01-01")
+    private LocalDate birthDate;
 
     @NotNull
     @JsonProperty
-    private String image; // 사진
+    @ApiModelProperty(value = "이미지", example = "image.png")
+    private String image;
 
     @NotNull
     @JsonProperty
-    private String content; // 배우 이력 내용
+    @ApiModelProperty(value = "이력 내용", example = "범죄도시 메인 배우")
+    private String content;
 
     public Actor toEntity() {
         return Actor.builder()
