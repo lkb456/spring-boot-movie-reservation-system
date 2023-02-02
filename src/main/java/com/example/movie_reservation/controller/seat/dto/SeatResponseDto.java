3e9ae960ce39bb3c2b565ce26c9a481f1924cf20
이경범 <tkdwk567@naver.com>
2023-02-02 17:02:12 +0900

    [remove] 좌석 예약 상태 enumType 정리

--- src/main/java/com/example/movie_reservation/controller/seat/dto/SeatResponseDto.java
+++ src/main/java/com/example/movie_reservation/controller/seat/dto/SeatResponseDto.java
@@ -1,6 +1,5 @@
 package com.example.movie_reservation.controller.seat.dto;
 
-import com.example.movie_reservation.domain.reservation.ReservationStatus;
 import com.example.movie_reservation.domain.seat.Seat;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,12 +11,10 @@ import lombok.RequiredArgsConstructor;
 public class SeatResponseDto {
 
     private final Integer seatNumber;
-    private final ReservationStatus reservationStatus;
 
     public static SeatResponseDto of(Seat seat) {
         return SeatResponseDto.builder()
                 .seatNumber(seat.getSeatNumber())
-                .reservationStatus(seat.getReservationStatus())
                 .build();
     }
 }
