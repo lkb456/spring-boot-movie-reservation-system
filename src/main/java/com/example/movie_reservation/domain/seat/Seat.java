9f67a697a65d413c47faeb0be7451a81da972cc6
이경범 <tkdwk567@naver.com>
2023-02-02 16:37:14 +0900

    [remove] enumType 불필요

--- src/main/java/com/example/movie_reservation/domain/seat/Seat.java
+++ src/main/java/com/example/movie_reservation/domain/seat/Seat.java
@@ -1,7 +1,6 @@
 package com.example.movie_reservation.domain.seat;
 
 import com.example.movie_reservation.domain.reservation.Reservation;
-import com.example.movie_reservation.domain.reservation.ReservationStatus;
 import lombok.*;
 
 import javax.persistence.*;
@@ -21,36 +20,19 @@ public class Seat {
     @Column(name = "SEAT_NUMBER")
     private Integer seatNumber;
 
-    @Enumerated(EnumType.STRING)
-    @Column(name = "STATUS")
-    private ReservationStatus reservationStatus; // 예약 상태
-
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "RESERVATIONS_ID")
     private Reservation reservation;
 
     @Builder
-    public Seat(Long id, Integer seatNumber, ReservationStatus reservationStatus, Reservation reservation) {
+    public Seat(Long id, Integer seatNumber, Reservation reservation) {
         this.id = id;
         this.seatNumber = seatNumber;
-        this.reservationStatus = reservationStatus;
         this.reservation = reservation;
     }
 
     public void reserve(Reservation reservation) {
         this.reservation = reservation;
         reservation.addSeat(this);
-    }
-
-    public void updateReserveStatus(ReservationStatus reservationStatus) {
-        this.reservationStatus = reservationStatus;
-    }
-
-    public boolean isAvailable() {
-        if (this.reservationStatus.equals(ReservationStatus.RESERVATION)) {
-            return false;
-        }
-
-        return true;
     }
 }
