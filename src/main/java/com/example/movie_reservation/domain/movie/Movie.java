59d701c079d85cb6d0cc05acae373f141624b4f3
이경범 <tkdwk567@naver.com>
2023-02-10 14:33:55 +0900

    [add] movie 개봉일 인덱스 설정
    movie 엔티티중 카디널리티가 가장 높은 속성이 개봉일이므로 인덱스를 설정하였다.

--- src/main/java/com/example/movie_reservation/domain/movie/Movie.java
+++ src/main/java/com/example/movie_reservation/domain/movie/Movie.java
@@ -1,4 +1,4 @@
-package com.example.movie_reservation.domain;
+package com.example.movie_reservation.domain.movie;
 
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 @EqualsAndHashCode(of = "id")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-@Table(name = "MOVIES")
+@Table(name = "MOVIES", indexes = @Index(columnList = "RELEASE_DATE"))
 public class Movie {
 
     @Id
@@ -21,13 +21,13 @@ public class Movie {
     @Column(name = "MOVIES_ID")
     private Long id; // pk
 
-    @Column(name = "TITLE")
+    @Column(name = "TITLE", nullable = false)
     private String title; // 제목
 
-    @Column(name = "RUNNING_TIME")
+    @Column(name = "RUNNING_TIME", nullable = false)
     private Duration runningTime; // 상영 시간
 
-    @Column(name = "RELEASE_DATE")
+    @Column(name = "RELEASE_DATE", nullable = false)
     private LocalDate releaseDate; // 개봉일
 
     @CreationTimestamp
